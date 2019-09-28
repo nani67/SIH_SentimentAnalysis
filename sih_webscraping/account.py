2fd2c1d746d848bab48afefc3c25190b2ca4c669
Kendrik Wah <48517661+kendrik-wah@users.noreply.github.com>
2019-09-28 21:56:13 +0100

    Add files via upload

--- sih_webscraping/account.py
+++ sih_webscraping/account.py
@@ -19,6 +19,7 @@ from nltk.classify import SklearnClassifier
 
 sid = SentimentIntensityAnalyzer()
 
+import numpy as np
 try:
     from PIL import Image
 except ImportError:
@@ -27,6 +28,25 @@ except ImportError:
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 path = r"C:\Users\kendrik\Pictures\sih_instagram\\"
+
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Fetch the service account key JSON file contents
+cred = credentials.Certificate('stressbuster.json')
+
+# Initialize the app with a service account, granting admin privileges
+firebase_admin.initialize_app(cred)
+
+# As an admin, the app has access to read and write all data, regradless of Security Rules
+ref = firestore.client()
+
+doc_ref = ref.collection(u'web-data-obtaining').document()
+
+users_ref = ref.collection(u'web-data-obtaining')
+docs = users_ref.stream()
 
 def read_json_file(filename):
     with open(filename) as json_file:
@@ -50,9 +70,34 @@ def get_reddit_json(file):
 def clean_tweet(tweet):
     return ' '.join(re.sub("(@(\w+))|([^0-9A-Za-z \t])|(\w+:\/\/\S+)|((http|https|ftp)://[a-zA-Z0-9\\./]+)|(#(\w+))"," ", tweet).split())
 
+def determine_risk(summarized_percentages):
+    if 0.10 <= summarized_percentages['negative'] <= 0.19:
+        return 'Low risk'
+    elif 0.2 <= summarized_percentages['negative'] <= 0.34:
+        return 'Medium risk'
+    elif 0.35 <= summarized_percentages['negative'] <= 0.49:
+        return 'High risk'
+    elif summarized_percentages['negative'] >= 0.5:
+        return 'Very high risk'
+    else:
+        return 'No risk'
+
+def process_img(im):
+    width = im.shape[1]
+    height = im.shape[0]
+    im = im.transpose(2,0,1).reshape(3,-1)
+
+    brg = np.amax(im,axis=0)
+    brg[brg==0] = 1
+    denom = np.sqrt((im[0]-im[1])**2-(im[0]-im[2])*(im[1]-im[2]))
+    # hue = np.arccos(0.5*(2*im[0]-im[1]-im[2])/denom)
+    sat = (brg - np.amin(im,axis=0))/brg
+
+    return width, height, np.mean(brg), np.mean(sat)
+
 class Account(object):
     def __init__(self):
-        self.instagram_username = ''
+        self.instagram_username = "kendrikwah"
         self.instagram_password = ''
         self.instagram_account = None
         self.twitter_username = ''
@@ -72,8 +117,9 @@ class Account(object):
         self.twitter_account = TwitterTarget(self.twitter_username)
 
     def extract_from_instagram(self):
-        instagram_results = self.instagram_account.scrape_self()
+        instagram_results = self.instagram_account.get_self_feed()
         self.instagram_filename = 'instagram_' + self.instagram_username + '.json'
+        return instagram_results
 
     def extract_from_instagram_offline(self):
         self.instagram_account.save_self_scraped_information()
@@ -81,14 +127,16 @@ class Account(object):
     def extract_from_twitter(self):
         twitter_results = self.twitter_account.scrape_self()
         self.twitter_filename = 'twitter_' + self.twitter_username + '.json'
+        return twitter_results
 
     def extract_from_twitter_offline(self):
         self.twitter_account.save_self_scraped_information()
 
     def analyze_instagram_feed(self, filename):
+        # self.extract_from_instagram_offline()
         extracted_json = get_instagram_json(filename)
         summary_text = {'negative': 0, 'positive': 0, 'neutral': 0}
-        summary_media = {'negative': 0, 'positive': 0, 'neutral': 0}
+        summary_media = {'negative': 0, 'not negative': 0}
         existing_text = []
         existing_media = []
 
@@ -98,8 +146,10 @@ class Account(object):
             tokenized_text = tokenize.sent_tokenize(text)
 
             for txt in tokenized_text:
+                txt = txt.lower()
                 if txt not in existing_text:
                     if txt is not None:
+                        txt = txt.lower()
                         ss = sid.polarity_scores(txt)
                         if ss['compound'] < 0:
                             summary_text['negative'] = summary_text['negative'] + 1
@@ -130,21 +180,46 @@ class Account(object):
 
                         existing_text.append(attempt_text)
 
+                    im = np.array(img)
+                    processing_result = process_img(im)
+                    if processing_result[2] <= 60 and processing_result[3] < 0.5:
+                        summary_media['negative'] = summary_media['negative'] + 1
+                    else:
+                        summary_media['not negative'] = summary_media['not negative'] + 1
                     existing_media.append(media)
 
         total = 0
         for base in list(summary_text.keys()):
             total += summary_text[base]
 
+        img_total = 0
+        for base in list(summary_media.keys()):
+            img_total += summary_media[base]
+
         for base in list(summary_text.keys()):
             summary_text[base] = round(summary_text[base]/total, 2)
 
-        return summary_text
+        for base in list(summary_media.keys()):
+            summary_media[base] = round(summary_media[base]/img_total, 2)
+
+        text_risk = determine_risk(summary_text)
+        media_risk = determine_risk(summary_media)
+
+        doc_ref.set({u'instagram_username': self.instagram_username,
+                     u'filename': filename,
+                     u'extracted_instagram_content': extracted_json,
+                     u'existing_instagram_feeds': existing_text,
+                     u'existing_instagram_media': existing_media,
+                     u'existing_text_count': summary_text,
+                     u'existing_media_count': summary_media,
+                     u'text_risk': text_risk,
+                     u'media_risk': media_risk})
 
     def analyze_twitter_feed(self, filename):
+        self.extract_from_twitter_offline()
         extracted_json = get_twitter_json(filename)
         summary_text = {'negative': 0, 'positive': 0, 'neutral': 0}
-        summary_media = {'negative': 0, 'positive': 0, 'neutral': 0}
+        summary_media = {'negative': 0, 'not_negative': 0}
         existing_text = []
         existing_media = []
 
@@ -155,6 +230,7 @@ class Account(object):
             if text not in existing_text:
                 if text is not None:
                     text = clean_tweet(text)
+                    text = text.lower()
                     ss = sid.polarity_scores(text)
                     if ss['compound'] < 0:
                         summary_text['negative'] = summary_text['negative'] + 1
@@ -184,44 +260,54 @@ class Account(object):
 
                         existing_text.append(attempt_text)
 
+                    im = np.array(img)
+                    processing_result = process_img(im)
+                    if processing_result[2] <= 20 and processing_result[3] <= 2:
+                        summary_media['negative'] = summary_media['negative'] + 1
+                    else:
+                        summary_media['not_negative'] = summary_media['not_negative'] + 1
+
                     existing_media.append(media)
 
         total = 0
         for base in list(summary_text.keys()):
             total += summary_text[base]
 
+        img_total = 0
+        for base in list(summary_media.keys()):
+            img_total += summary_media[base]
+
         for base in list(summary_text.keys()):
             summary_text[base] = round(summary_text[base]/total, 2)
 
-        return summary_text
+        for base in list(summary_media.keys()):
+            summary_media[base] = round(summary_media[base]/img_total, 2)
 
-    def determine_risk(self, summarized_percentages):
-        if 0.10 <= summarized_percentages['negative'] <= 0.19:
-            return 'Low risk'
-        elif 0.2 <= summarized_percentages['negative'] <= 0.34:
-            return 'Medium risk'
-        elif 0.35 <= summarized_percentages['negative'] <= 0.49:
-            return 'High risk'
-        elif summarized_percentages['negative'] >= 0.5:
-            return 'Very high risk'
-        else:
-            return 'No risk'
+        text_risk = determine_risk(summary_text)
+        media_risk = determine_risk(summary_media)
+
+        doc_ref.set({u'twitter_username': self.twitter_username,
+                     u'filename': filename,
+                     u'extracted_twitter_content': extracted_json,
+                     u'existing_twitter_texts': existing_text,
+                     u'existing_twitter_media': existing_media,
+                     u'existing_text_count': summary_text,
+                     u'existing_media_count': summary_media,
+                     u'text_risk': text_risk,
+                     u'media_risk': media_risk})
 
 
 acc = Account()
-# acc.log_into_instagram("xxxxxxxxxx", "xxxxxxxxxxx")
+# acc.log_into_instagram("xxxxxx", "xxxxxxxx")
 # acc.extract_from_instagram_offline()
-extracted_instagram = acc.determine_risk(acc.analyze_instagram_feed('instagram_kendrikwah.json'))
+extracted_instagram = acc.analyze_instagram_feed("instagram_kendrikwah.json")
+
 # acc.log_into_twitter("depressingmsgs")
 # acc.extract_from_twitter_offline()
-extracted_twitter = acc.determine_risk(acc.analyze_twitter_feed('twitter_depressingmsgs.json'))
+# extracted_twitter = acc.analyze_twitter_feed('twitter_depressingmsgs.json')
 
-print('Instagram:', extracted_instagram)
-print('Twitter:', extracted_twitter)
 #
 #    Pipeline will work like this:
 # 1) Get credentials of users (username and password for Instagram, username for Twitter)
-# 2) Upon login, can immediately do extract_from_instagram() and extract_from_twitter()
-# 3) After doing extraction, use analyze_instagram_feed() and analyze_twitter_feed() (use offline methods if you wish to extract via json instead)
+# 2) After doing extraction, use analyze_instagram_feed() and analyze_twitter_feed() (use offline methods if you wish to extract via json instead)
 #    These analyze methods will have the NLP and image-to-string codes inside.
-# 4) use determine_risk() to find out the person's risk of getting depression!
